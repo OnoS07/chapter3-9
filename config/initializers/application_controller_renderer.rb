e4a7f2f5eee66ef0bd114f0e3177830963c7f0f6
大野颯一郎 <onosoichiro@tapio.local>
2020-04-12 18:15:47 +0900

    [Finish]3-9

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '36cbcb902f5f0618730771894d3c0fceded5f05dac6f226fab6f6b44cdec541ad19b13b04def3d9b0c86e7aeb55de45ffeb8db4bfbb2c75ab4d86ec377f97c73'
